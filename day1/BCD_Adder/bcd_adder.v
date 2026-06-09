f2bd925f61ffa3b290a3a2994d0ff80c33cc199c
akkuasif <24bl16505@rit.ac.in>
2026-06-09 21:39:44 +0530

    Update bcd_adder.v

--- day1/BCD_Adder/bcd_adder.v
+++ day1/BCD_Adder/bcd_adder.v
@@ -1,1 +1,36 @@
 
+module bcd_adder(
+    input [3:0] a, 
+    input [3:0] b,
+    input cin,
+    output [3:0] sum,
+    output cout
+    );
+    
+    wire [3:0] sum1;        
+    wire cout1;             
+    wire correction_carry;  
+    wire ignore_cout;
+    
+    rca_4bit rca1 (
+        .a(a),
+        .b(b),
+        .cin(cin),
+        .sum(sum1),
+        .cout(cout1)
+    );
+    
+        assign correction_carry = cout1 | (sum1[3] & sum1[2]) | (sum1[3] & sum1[1]);
+        wire [3:0] correction_word = {1'b0, correction_carry, correction_carry, 1'b0};
+        
+    rca_4bit rca2 (
+        .a(sum1),
+        .b(correction_word),
+        .cin(1'b0),          
+        .sum(sum),
+        .cout(ignore_cout)  
+    );
+    
+    assign cout = correction_carry;
+
+endmodule
