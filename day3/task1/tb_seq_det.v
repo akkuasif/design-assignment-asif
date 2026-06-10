d8ede7ba870c5fb28573b22efd9d3194056dc9de
akkuasif <24bl16505@rit.ac.in>
2026-06-10 15:00:01 +0530

    Update tb_seq_det.v

--- day3/task1/tb_seq_det.v
+++ day3/task1/tb_seq_det.v
@@ -1,1 +1,43 @@
 
+`timescale 1ns / 1ps
+
+module tb_sequence_detector;
+
+    reg clk;
+    reg reset;
+    reg sequence_in;
+    wire detector_out;
+
+    sequence_detector_1110 uut (
+        .clk(clk), 
+        .reset(reset), 
+        .sequence_in(sequence_in), 
+        .detector_out(detector_out)
+    );
+
+    always #10 clk = ~clk;
+
+    initial begin
+        clk = 0;
+        reset = 1;
+        sequence_in = 0;
+        #40;
+        reset = 0;
+        
+        
+        @(negedge clk); sequence_in = 1; 
+        @(negedge clk); sequence_in = 1; 
+        @(negedge clk); sequence_in = 1; 
+        @(negedge clk); sequence_in = 0; 
+        
+        @(negedge clk); sequence_in = 1; 
+        @(negedge clk); sequence_in = 1; 
+        @(negedge clk); sequence_in = 1; 
+        @(negedge clk); sequence_in = 1; 
+        @(negedge clk); sequence_in = 0; 
+
+        #40;
+        $finish;
+    end
+      
+endmodule
