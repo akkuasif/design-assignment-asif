83556d83300f5e424bed755b3f0baf85f50a0b7a
akkuasif <24bl16505@rit.ac.in>
2026-06-17 21:39:50 +0530

    Update interface.sv

--- DAY8/VERIFICATION/interface.sv
+++ DAY8/VERIFICATION/interface.sv
@@ -1,1 +1,23 @@
 
+interface apb_if(input bit PCLK);
+
+logic PRESETn;
+
+logic start;
+logic rw;
+logic [7:0] addr;
+logic [31:0] wdata;
+
+logic PSEL;
+logic PENABLE;
+logic PWRITE;
+logic [7:0] PADDR;
+logic [31:0] PWDATA;
+
+logic [31:0] PRDATA;
+logic [31:0] rdata;
+
+logic PREADY;
+logic done;
+
+endinterface
