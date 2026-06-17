fcb665e6a9ab54e9f6d744bc481c10ab1eab4df0
akkuasif <24bl16505@rit.ac.in>
2026-06-17 21:57:12 +0530

    Update env.sv

--- DAY8/VERIFICATION/env.sv
+++ DAY8/VERIFICATION/env.sv
@@ -1,1 +1,23 @@
+class env;
 
+agent agt;
+scoreboard sb;
+
+function new(agent agt,
+             scoreboard sb);
+
+ this.agt = agt;
+ this.sb  = sb;
+
+endfunction
+
+task run();
+
+ fork
+  agt.run();
+  sb.run();
+ join_none
+
+endtask
+
+endclass
